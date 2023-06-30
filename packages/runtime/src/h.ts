1133db6b90f1454f21f91418b7070ed63f79c74b
saifaldin14 <saifaldindeveloper@gmail.com>
2023-06-30 19:24:08 -0400

    Set up virtual node

---
+++ packages/runtime/src/h.ts
@@ -0,0 +1,8 @@
+import { DomNodeType } from "./types";
+
+// Define the Virtual DOM HTML Elements
+export const DOM_TYPES: DomNodeType = {
+  TEXT: "text",
+  ELEMENT: "element",
+  FRAGMENT: "fragment",
+};
---
+++ packages/runtime/src/types.ts
@@ -0,0 +1,5 @@
+export type DomNodeType = {
+  TEXT: string;
+  ELEMENT: string;
+  FRAGMENT: string;
+};
