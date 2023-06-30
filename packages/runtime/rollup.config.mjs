9bf1cee4e17f95c786a584c19fdd29dd2407a911
saifaldin14 <saifaldindeveloper@gmail.com>
2023-06-30 18:14:05 -0400

    Create runtime package

---
+++ packages/runtime/rollup.config.mjs
@@ -0,0 +1,13 @@
+import cleanup from "rollup-plugin-cleanup";
+import filesize from "rollup-plugin-filesize";
+export default {
+  input: "src/index.ts",
+  plugins: [cleanup()],
+  output: [
+    {
+      file: "dist/veritas.ts",
+      format: "esm",
+      plugins: [filesize()],
+    },
+  ],
+};
