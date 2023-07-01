b06dd9b9f5aa6989d82ea2f79b620df81f13086f
saifaldin14 <saifaldindeveloper@gmail.com>
2023-07-01 07:23:43 -0400

    Create elementTypes.ts

---
+++ packages/runtime/src/types/elementTypes.ts
@@ -0,0 +1,34 @@
+import { VNode } from "./common";
+
+/* eslint-disable @typescript-eslint/ban-types */
+type PropsStyleType = {
+  prop1: string;
+  prop2: string;
+};
+
+type ElementFunctionType = {
+  prop1: string;
+  prop2: Function;
+};
+
+export type ElementNodePropsType = {
+  on: ElementFunctionType;
+  class: string | string[];
+  style: PropsStyleType;
+};
+
+export type ElementNodeType = {
+  type: string;
+  tag: string;
+  props: ElementNodePropsType;
+  children: VNode[];
+};
+
+export type vElementNode = {
+  type: string;
+  tag: string;
+  props: ElementNodePropsType;
+  children: VNode[];
+  el: HTMLElement;
+  listeners: ElementFunctionType;
+};
