648df9216617da5f43d3497c5c209ca250c68969
saifaldin14 <saifaldindeveloper@gmail.com>
2023-07-01 06:54:29 -0400

    Update dispatcher.ts

--- packages/runtime/src/dispatcher.ts
+++ packages/runtime/src/dispatcher.ts
@@ -1,6 +1,9 @@
+import { VoidFunctionType } from "./types";
+
 /* eslint-disable @typescript-eslint/no-empty-function */
 export class Dispatcher {
-  #subs = new Map();
+  #subs = new Map(); // Store the registered handlers by event name
+  #afterHandlers: VoidFunctionType[] = [];
 
   /**
    * Registers an handler function that executes in response to a specific command
@@ -10,7 +13,10 @@ export class Dispatcher {
    * @param {(any) => void} handler the handler of the command
    * @returns {() => void} a function that un-registers the handler
    */
-  subscribe = (commandName: string, handler: (any) => void): (() => void) => {
+  subscribe = (
+    commandName: string,
+    handler: (any) => void
+  ): VoidFunctionType => {
     if (!this.#subs.has(commandName)) {
       this.#subs.set(commandName, []);
     }
@@ -25,6 +31,22 @@ export class Dispatcher {
     return () => {
       const idx = handlers.indexOf(handler);
       handlers.splice(idx, 1);
+    };
+  };
+
+  /**
+   * Registers a handler function that runs after each command and returns a
+   * function that un-registers the handler.
+   *
+   * @param {() => void} handler a function that runs after each command
+   * @returns {() => void} a function that un-registers the handler
+   */
+  afterEveryCommand = (handler: VoidFunctionType): VoidFunctionType => {
+    this.#afterHandlers.push(handler);
+
+    return () => {
+      const idx = this.#afterHandlers.indexOf(handler);
+      this.#afterHandlers.splice(idx, 1);
     };
   };
 }
