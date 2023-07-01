8d987ec7122924e8362475161555e3970c66cdc9
saifaldin14 <saifaldindeveloper@gmail.com>
2023-06-30 20:17:50 -0400

    Update arrays.ts

--- packages/runtime/src/utils/arrays.ts
+++ packages/runtime/src/utils/arrays.ts
@@ -1,3 +1,10 @@
+/**
+ * Filters NULL nodes an array
+ * Used for conditional rendering where we don't want to render empty nodes
+ * NULL nodes shouldn't be added to the DOM
+ * @param arr the child nodes to evaluate
+ * @returns a filtered array without NULL values
+ */
 export function withoutNulls(arr) {
   return arr.filter((item) => item != null);
 }
