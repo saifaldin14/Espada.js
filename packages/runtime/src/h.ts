22b547c0786ea0ea97c8481908e8a78b43e50c0e
saifaldin14 <saifaldindeveloper@gmail.com>
2023-06-30 20:12:10 -0400

    Update h.ts

--- packages/runtime/src/h.ts
+++ packages/runtime/src/h.ts
@@ -1,25 +1,42 @@
-import { DomNodeType, HyperTextNodeType, HyperTextPropsType } from "./types";
+import {
+  DomNodeType,
+  HyperTextChildNodeType,
+  HyperTextNodeType,
+  HyperTextPropsType,
+} from "./types";
 import { withoutNulls } from "./utils/arrays";
 
-// Define the Virtual DOM HTML Elements
+/**
+ * Defines the Virtual DOM tree node types
+ * Text: for empty or value based nodes
+ * Element: Most HTML nodes (div, p, button, ul, etc.)
+ * Fragment: Empty tags <></> used to house other elements within it
+ */
 export const DOM_TYPES: DomNodeType = {
   TEXT: "text",
   ELEMENT: "element",
   FRAGMENT: "fragment",
 };
 
-export function h(
+/**
+ * Defines HTML Element Nodes
+ * @param tag string, tag of the node
+ * @param props HyperTextPropsType, the props of the element
+ * @param children HyperTextChildNodeType[], the children nodes
+ * @returns HyperTextNodeType, the HTML Element
+ */
+export const h = (
   tag: string,
   props: HyperTextPropsType = {},
-  children = []
-): HyperTextNodeType {
+  children: HyperTextChildNodeType[] = []
+): HyperTextNodeType => {
   return {
     tag,
     props,
     children: mapTextNodes(withoutNulls(children)),
     type: DOM_TYPES.ELEMENT,
   };
-}
+};
 
 export const mapTextNodes = (children) => {
   return children.map((child) =>
