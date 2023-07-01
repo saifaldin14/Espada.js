1950aaecb5edf6beffbfc12be9ab1a72964b0eae
saifaldin14 <saifaldindeveloper@gmail.com>
2023-07-01 06:55:29 -0400

    Update dispatcher.ts

--- packages/runtime/src/dispatcher.ts
+++ packages/runtime/src/dispatcher.ts
@@ -1,6 +1,7 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-empty-function */
 import { VoidFunctionType } from "./types";
 
-/* eslint-disable @typescript-eslint/no-empty-function */
 export class Dispatcher {
   #subs = new Map(); // Store the registered handlers by event name
   #afterHandlers: VoidFunctionType[] = [];
@@ -49,4 +50,23 @@ export class Dispatcher {
       this.#afterHandlers.splice(idx, 1);
     };
   };
+
+  /**
+   * Dispatches a command to all registered handlers and runs all
+   * handlers registered to run after each command.
+   *
+   * Displays a warning if the command has no handlers registered.
+   *
+   * @param {string} commandName the name of the command to dispatch
+   * @param {any} payload the payload of the command
+   */
+  dispatch(commandName: string, payload: any) {
+    if (this.#subs.has(commandName)) {
+      this.#subs.get(commandName).forEach((handler) => handler(payload));
+    } else {
+      console.warn(`No handlers for command: ${commandName}`);
+    }
+
+    this.#afterHandlers.forEach((handler) => handler());
+  }
 }
