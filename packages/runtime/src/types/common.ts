1184ad5a5210147780d29402b707f631c527753b
saifaldin14 <saifaldindeveloper@gmail.com>
2023-07-01 17:23:06 -0400

    Update common.ts

--- packages/runtime/src/types/common.ts
+++ packages/runtime/src/types/common.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { vElementNode } from "./elementTypes";
 import { vFragmentNode } from "./fragmentTypes";
 import { vTextNode } from "./textTypes";
@@ -6,3 +7,9 @@ export type VNode = vTextNode | vElementNode | vFragmentNode;
 export type VoidFunctionType = () => void;
 export type ValueType = string | number | null;
 export type EventHandlerType = (event: Event) => void;
+export type ArraysDiffSequenceOp = {
+  op: string;
+  from: number;
+  index: number;
+  item: any | undefined;
+};
