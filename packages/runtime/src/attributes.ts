0cbd81fa5ece9aab044f545d2c4cc3583583fd2c
saifaldin14 <saifaldindeveloper@gmail.com>
2023-06-30 22:31:17 -0400

    Update attributes.ts

--- packages/runtime/src/attributes.ts
+++ packages/runtime/src/attributes.ts
@@ -1,4 +1,13 @@
-export function setAttributes(el, attrs) {
+/**
+ * Extracts the attributes that require special handling
+ * (the style and class attributes) from the rest of the
+ * attributes and then call the setStyle() and setClass()
+ * functions to set those attributes, rest are passed to
+ * setAttribute()
+ * @param el
+ * @param attrs
+ */
+export const setAttributes = (el, attrs) => {
   const { class: className, style, ...otherAttrs } = attrs;
 
   if (className) {
@@ -14,39 +23,72 @@ export function setAttributes(el, attrs) {
   for (const [name, value] of Object.entries(otherAttrs)) {
     setAttribute(el, name, value);
   }
-}
+};
 
-function setClass(el, className) {
+/**
+ * Sets the class attribute
+ * @param el
+ * @param className
+ */
+const setClass = (el, className) => {
   el.className = "";
 
+  // The DOM Element doesn't have a class property
+  // Instead it has className and classList
+  // Allows users to set the class attribute in two ways:
+  // Either as a string or as an array of string items
   if (typeof className === "string") {
-    el.className = className;
+    el.className = className; // Detect single className and add it
   }
 
   if (Array.isArray(className)) {
-    el.classList.add(...className);
+    el.classList.add(...className); // Detects that className is an array and adds it to classList
   }
-}
+};
 
-export function setStyle(el, name, value) {
+/**
+ * Sets user-defined style on the DOM element
+ * @param el
+ * @param name
+ * @param value
+ */
+export const setStyle = (el, name, value) => {
   el.style[name] = value;
-}
+};
 
-export function removeStyle(el, name) {
+/**
+ * Removes user-defined style from the DOM element
+ * @param el
+ * @param name
+ */
+export const removeStyle = (el, name) => {
   el.style[name] = null;
-}
+};
 
-export function setAttribute(el, name, value) {
+/**
+ * Handles setting the attributes for everything other than
+ * classes and styles
+ * @param el
+ * @param name
+ * @param value
+ */
+export const setAttribute = (el, name, value) => {
   if (value == null) {
+    // Null value, remove it from the DOM
     removeAttribute(el, name);
   } else if (name.startsWith("data-")) {
     el.setAttribute(name, value);
   } else {
     el[name] = value;
   }
-}
+};
 
-export function removeAttribute(el, name) {
+/**
+ * Removes the attribute from the DOM Element if the value is NULL
+ * @param el
+ * @param name
+ */
+export const removeAttribute = (el, name) => {
   el[name] = null;
   el.removeAttribute(name);
-}
+};
