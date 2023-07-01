042eb28d9f826d98017233811f01f0c0b60f0a42
saifaldin14 <saifaldindeveloper@gmail.com>
2023-06-30 22:13:33 -0400

    Update events.ts

--- packages/runtime/src/events.ts
+++ packages/runtime/src/events.ts
@@ -1,8 +1,22 @@
+/**
+ * Adds events since the element node is an instance of the EventTarget interface
+ * Implemented by all the DOM nodes that can receive events
+ * @param eventName
+ * @param handler
+ * @param el
+ * @returns
+ */
 export function addEventListener(eventName, handler, el) {
   el.addEventListener(eventName, handler);
   return handler;
 }
 
+/**
+ * Adds multiple event listeners in the form of an object to an element node
+ * @param listeners
+ * @param el
+ * @returns
+ */
 export function addEventListeners(listeners = {}, el) {
   const addedListeners = {};
 
