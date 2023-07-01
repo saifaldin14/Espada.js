a0016cf3b31dde096cc06018b92491a6acf77059
saifaldin14 <saifaldindeveloper@gmail.com>
2023-06-30 20:07:13 -0400

    Update types.ts

--- packages/runtime/src/types.ts
+++ packages/runtime/src/types.ts
@@ -3,3 +3,26 @@ export type DomNodeType = {
   ELEMENT: string;
   FRAGMENT: string;
 };
+
+export type HyperTextPropsType = {
+  type?: string;
+  class?: string;
+};
+
+export type HyperTextChildPropsType = {
+  type?: string;
+  name?: string;
+};
+
+export type HyperTextChildNodeType = {
+  type?: string;
+  tag?: string;
+  props?: HyperTextChildPropsType;
+};
+
+export type HyperTextNodeType = {
+  type: string;
+  tag: string;
+  props: HyperTextPropsType;
+  children: HyperTextChildNodeType[];
+};
