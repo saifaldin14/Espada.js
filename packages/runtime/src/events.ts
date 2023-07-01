bb70ac00aa6ae83a30dfe477297ffb7e64787788
saifaldin14 <saifaldindeveloper@gmail.com>
2023-07-01 07:49:37 -0400

    Update events.ts

--- packages/runtime/src/events.ts
+++ packages/runtime/src/events.ts
@@ -1,23 +1,34 @@
+import { EventHandlerType } from "./types/common";
+
 /**
- * Adds events since the element node is an instance of the EventTarget interface
- * Implemented by all the DOM nodes that can receive events
- * @param eventName
- * @param handler
- * @param el
- * @returns
+ * Adds an event listener to an event target and returns the listener.
+ *
+ * @param {string} eventName the name of the event to listen to
+ * @param {EventHandlerType} handler the event handler
+ * @param {EventTarget} el the element to add the event listener to
+ * @returns {EventHandlerType} the event handler
  */
-export const addEventListener = (eventName, handler, el) => {
+export const addEventListener = (
+  eventName: string,
+  handler: EventHandlerType,
+  el: EventTarget
+): EventHandlerType => {
   el.addEventListener(eventName, handler);
   return handler;
 };
 
 /**
- * Adds multiple event listeners in the form of an object to an element node
- * @param listeners
- * @param el
- * @returns
+ * Adds event listeners to an event target and returns an object containing
+ * the added listeners.
+ *
+ * @param {object} listeners The event listeners to add
+ * @param {EventTarget} el The element to add the listeners to
+ * @returns {object} The added listeners
  */
-export const addEventListeners = (listeners = {}, el) => {
+export const addEventListeners = (
+  listeners: object = {},
+  el: EventTarget
+): object => {
   const addedListeners = {};
 
   Object.entries(listeners).forEach(([eventName, handler]) => {
@@ -29,12 +40,15 @@ export const addEventListeners = (listeners = {}, el) => {
 };
 
 /**
- * Given the names of the event listeners,
- * removes them from the DOM Element
- * @param listeners
- * @param el
+ * Removes the event listeners from an event target.
+ *
+ * @param {object} listeners the event listeners to remove
+ * @param {EventTarget} el the element to remove the event listeners from
  */
-export const removeEventListeners = (listeners = {}, el) => {
+export const removeEventListeners = (
+  listeners: object = {},
+  el: EventTarget
+) => {
   Object.entries(listeners).forEach(([eventName, handler]) => {
     el.removeEventListener(eventName, handler);
   });
